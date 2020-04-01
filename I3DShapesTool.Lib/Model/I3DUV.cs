fd988b2f0a09c3fe39be30462bdc7be9ebe12acb
Marc Robards <marc@robardsoft.com>
2020-04-01 10:42:13 -0400

    handled zero-length error

--- I3DShapesTool.Lib/Model/I3DUV.cs
+++ I3DShapesTool.Lib/Model/I3DUV.cs
@@ -2,23 +2,41 @@
 
 namespace I3DShapesTool.Lib.Model
 {
-    public class I3DUV
-    {
-        public float U { get; }
-        public float V { get; }
+  public class I3DUV
+  {
+    public float U { get; }
+    public float V { get; }
 
-        public I3DUV(BinaryReader br, int fileVersion)
+    public I3DUV(BinaryReader br, int fileVersion)
+    {
+      if (fileVersion >= 4) // Can be 5 as well
+      {
+        //V = br.ReadSingle();
+        //U = br.ReadSingle();
+
+        // handling zero-length data
+        try
         {
-            if (fileVersion >= 4) // Can be 5 as well
-            {
-                V = br.ReadSingle();
-                U = br.ReadSingle();
-            }
-            else
-            {
-                U = br.ReadSingle();
-                V = br.ReadSingle();
-            }
+          V = br.ReadSingle();
         }
+        catch (System.Exception)
+        {
+          V = 0;
+        }
+        try
+        {
+          U = br.ReadSingle();
+        }
+        catch (System.Exception)
+        {
+          U = 0;
+        }
+      }
+      else
+      {
+        U = br.ReadSingle();
+        V = br.ReadSingle();
+      }
     }
+  }
 }
